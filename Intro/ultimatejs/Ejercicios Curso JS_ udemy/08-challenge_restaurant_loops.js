9080f6f19d4202dd7bb60edaef140a9d92139139
ISM42 <133287937+ISM42@users.noreply.github.com>
2024-05-17 23:28:26 -0600

    Avance 07_ curso JS

--- Intro/ultimatejs/Ejercicios Curso JS_ udemy/08-challenge_restaurant_loops.js
+++ Intro/ultimatejs/Ejercicios Curso JS_ udemy/08-challenge_restaurant_loops.js
@@ -22,36 +22,33 @@ BONUS:
 */
 
 const calcTip = function (bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  tips[i] = calcTip(bills[i]);
+  totals[i] = tips[i] + bills[i];
+}
+console.log(bills);
+console.log(tips);
+console.log(totals);
+
+//BONUS
+
+const arr = [5, 5, 5];
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
   }
-  
-  const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-  
-  const tips = [];
-  const totals = [];
-  
-  for (let i = 0; i < bills.length; i++){
-      tips[i] = calcTip(bills[i]);
-      totals[i] = tips[i] + bills[i];
-  }
-  console.log(bills);
-  console.log(tips);
-  console.log(totals);
+  console.log(sum);
+  return sum / arr.length;
+};
 
-
-
-  //BONUS
-
-  const arr = [5,5,5];
-
-  const calcAverage = function(arr){
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++){
-        sum = sum + arr[i];
-    }
-    console.log(sum);
-    return sum / arr.length;
-  }
-
-  console.log(calcAverage(arr));
-
+console.log(calcAverage(arr));
--- Intro/ultimatejs/JS Course_Udemy/functions.js
+++ Intro/ultimatejs/JS Course_Udemy/functions.js
@@ -3,12 +3,12 @@
 
 Functions are a piece of code that we can reuse over and over again in our code.*/
 
-//Syntax:
-	function NAME(){
-	CODE //BODY
-	}
+/*Syntax:
+function NAME() {
+  CODE; //BODY
+}
 
-NAME(); //how we call or invoke the function
+NAME(); //how we call or invoke the function*/
 
 /*Usually when we write functions we also pass data into a function and additionally, a function can also return data as well which means to give us data back that we can then use for something else in the program.
 
@@ -21,34 +21,33 @@ The RETURN statement is used to output a value from the function and terminate e
 	
 	Ej: */
 
-	function fruitProcessor(apples, oranges) { //estos parámetros tendrán valores numéricos
-	console.log(apples, oranges);
-	const juice = `Juice with ${apples} apples and ${oranges} oranges.`; 
-	return juice;
-	}
+function fruitProcessor(apples, oranges) {
+  //estos parámetros tendrán valores numéricos
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
 
-	fruitProcessor(5,0); //aquí definimos los valores de los parámetros. Es lo único que recibimos como salida (5 y 0). Para ver el resultado de JUICE (lo que retorna esta función) tenemos dos opciones:
+fruitProcessor(5, 0); //aquí definimos los valores de los parámetros. Es lo único que recibimos como salida (5 y 0). Para ver el resultado de JUICE (lo que retorna esta función) tenemos dos opciones:
 
-	console.log(fruitProcessor(5,0)); // vemos el valor de JUICE directamente
+console.log(fruitProcessor(5, 0)); // vemos el valor de JUICE directamente
 
 //O
 
-	const appleJuice = fruitProcessor(5,0);
-	console.log(appleJuice); //guardamos lo que retorna nuestra función en una nueva variable y después la loggeamos para imprimir su resultado.
-
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice); //guardamos lo que retorna nuestra función en una nueva variable y después la loggeamos para imprimir su resultado.
 
 /*Thanks to the power of the function we can reuse the function with different input values and the get a different output.
 
 	Ej: now we create a new variable where we store the new result:*/
-	
-	const appleOrangeJuice = fruitProcessor(2,4);
-	console.log(appleOrangeJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
 
 /*We can see the functions as a machine.
 
 Not all functions need to return something and not all funtions need to accept parameters.
 */
-
 
 // 										    			FUNCTION EXPRESSIONS
 
@@ -60,24 +59,84 @@ function does not have a name (it's an anonymous function).
 This lines are an expression and an expression produces a value.
 
 				Ej:
-*/                 const calcAge2 = function(birthYear){
-					return 2037 -  birthYear;
-					}
+*/ const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
 // To call a function expression we do it in the same way:
-					const age2 = calcAge2(1991);
-					console.log(age2);
+const age2 = calcAge2(1991);
+console.log(age2);
 
-//another difference between function declarations and function expressions, is that we can call the function declarations before they 
+//another difference between function declarations and function expressions, is that we can call the function declarations before they
 //are defined in the code.
 
-					//Ej:
-						const age1 = calcAge1(1994);
+//Ej:
+const age1 = calcAge1(1994);
 
-						function calcAge1(birthYear){
-							return 2037 - birthYear;
-						}   //this works. If we do the same with a function expression, that does not work.
-							//Anyway, it's not a good idea to call a function before it is created in many cases.
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+} //this works. If we do the same with a function expression, that does not work.
+//Anyway, it's not a good idea to call a function before it is created in many cases.
 
-						//It is a personal preference which type of function is better to use.
+//It is a personal preference which type of function is better to use.
 
-//NOTE: THE RETURN STATEMENT IMMEDIATLY EXITS THE FUNCTION. SO, EVERY LINE AFTER A "RETURN" STATEMENT WILL NOT BE EXECUTED. 
+//NOTE: THE RETURN STATEMENT IMMEDIATLY EXITS THE FUNCTION. SO, EVERY LINE AFTER A "RETURN" STATEMENT WILL NOT BE EXECUTED.
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+// EXAMPLE: CALCULATING MAX AND MIN IN A ARRAY
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0]; //asumimos que le primer elemento es el valor máximo antes de empezar a iterar sobre el arreglo
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    if (typeof temps[i] !== "number") continue; //ignorando si existen errores en la lectura de las temperaturas guardadas en el arreglo con el sensor
+
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+//calcTempAmplitude([3, 7, 4, 23]);
+
+const amplitude = calcTempAmplitude(temperatures);
+//calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+//what if we need to use 2 arrays to calculate the amplitude? We need to merge both arrays, so we can use the same function only once
+
+//can go to Mozilla Developer Docs to see the methods that can be used in arrays. In this case we need CONCAT
+
+//Example CONCAT:
+
+const array1 = ["a", "b", "c"];
+const array2 = ["d", "e", "f"];
+
+const array3 = array1.concat(array2);
+console.log(array3);
+
+//we pass both array in a new function like this:
+
+const calcTempAmplitudNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0]; //asumimos que le primer elemento es el valor máximo antes de empezar a iterar sobre el arreglo
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    if (typeof temps[i] !== "number") continue; //ignorando si existen errores en la lectura de las temperaturas guardadas en el arreglo con el sensor
+
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeNew = calcTempAmplitudNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
