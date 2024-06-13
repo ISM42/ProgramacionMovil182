2a07b687a3c362d2ce13cb93d2020a513ecdd18c
ISM42 <133287937+ISM42@users.noreply.github.com>
2024-06-12 19:22:35 -0600

    Fin de práctica 6: componentes, props y states

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react"; //importar el uso de estado
 import { StyleSheet, Text, View, Button } from "react-native";
+
+//función donde creamos nuestro componente
+const Texto = () => {
+  const [contenido, setContenido] = useState("holaMundo");
+  const actualizarContenido = () => {
+    setContenido("State actualizó este texto");
+  };
+  return <Text onPress={actualizarContenido}>{contenido}</Text>;
+};
+
+const Boton = () => {
+  const [contenido, setContenido] = useState("Presioname");
+  const actualizarContenido = () => {
+    setContenido("State actualizó este boton");
+  };
+  return (
+    <Button title={contenido} color="#000000" onPress={actualizarContenido}>
+      {contenido}
+    </Button>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola mundo!</Text>
-      <button title=" Presióname"></button>
+      <Texto contenido={"Hola mundo"}></Texto>
+      <Texto contenido={"ReactNative"} />
+
+      <Boton></Boton>
+
       <StatusBar style="auto" />
     </View>
   );
