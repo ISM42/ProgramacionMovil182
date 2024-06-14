55974bab2a27b5dba9a2833935628904f89ab4b3
ISM42 <133287937+ISM42@users.noreply.github.com>
2024-06-14 15:26:16 -0600

    Práctica 7: stylesheet y flex

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -3,12 +3,16 @@ import React, { useState } from "react"; //importar el uso de estado
 import { StyleSheet, Text, View, Button } from "react-native";
 
 //función donde creamos nuestro componente
-const Texto = () => {
+const Texto = ({ estilo }) => {
   const [contenido, setContenido] = useState("holaMundo");
   const actualizarContenido = () => {
     setContenido("State actualizó este texto");
   };
-  return <Text onPress={actualizarContenido}>{contenido}</Text>;
+  return (
+    <Text style={[styles.text, estilo]} onPress={actualizarContenido}>
+      {contenido}
+    </Text>
+  );
 };
 
 const Boton = () => {
@@ -26,11 +30,9 @@ const Boton = () => {
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto contenido={"Hola mundo"}></Texto>
-      <Texto contenido={"ReactNative"} />
-
-      <Boton></Boton>
-
+      <Texto estilo={styles.red} />
+      <Texto estilo={styles.green} />
+      <Texto estilo={styles.blue} />
       <StatusBar style="auto" />
     </View>
   );
@@ -39,8 +41,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column-reverse",
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: "center", //tiene diferentes propiedades. El predeterminado es "center" (izquiera a derecha)
+    justifyContent: "space-around", //tiene diferentes propiedades. El predeterminado es "center" (arriba a abajo)
   },
+
+  text: {
+    color: "yellow",
+    fontSize: 25,
+    //height: 150,
+    // width: 150,
+  },
+
+  red: { backgroundColor: "red" /*flex: 1*/ },
+  green: { backgroundColor: "green" /*flex: 2*/ },
+  blue: { backgroundColor: "blue" /*flex: 3*/ },
 });
