f8e19216efb8971832fedbaf8b98f687c83f5d3c
ISM42 <133287937+ISM42@users.noreply.github.com>
2024-06-18 09:19:58 -0600

    Práctica 8: textInput y botones

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -1,38 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react"; //importar el uso de estado
-import { StyleSheet, Text, View, Button } from "react-native";
-
-//función donde creamos nuestro componente
-const Texto = ({ estilo }) => {
-  const [contenido, setContenido] = useState("holaMundo");
-  const actualizarContenido = () => {
-    setContenido("State actualizó este texto");
-  };
-  return (
-    <Text style={[styles.text, estilo]} onPress={actualizarContenido}>
-      {contenido}
-    </Text>
-  );
-};
-
-const Boton = () => {
-  const [contenido, setContenido] = useState("Presioname");
-  const actualizarContenido = () => {
-    setContenido("State actualizó este boton");
-  };
-  return (
-    <Button title={contenido} color="#000000" onPress={actualizarContenido}>
-      {contenido}
-    </Button>
-  );
-};
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const onPress = () => {
+    setCount(count + 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Texto estilo={styles.red} />
-      <Texto estilo={styles.green} />
-      <Texto estilo={styles.blue} />
+      <View style={styles.countContainer}>
+        <Text style={styles.countText}> Número de toques: {count}</Text>
+      </View>
+      <TouchableWithoutFeedback onPress={onPress}>
+        <View style={styles.button}>
+          <Text>Toca aquí</Text>
+        </View>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </View>
   );
@@ -41,20 +27,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column-reverse",
     backgroundColor: "#fff",
-    alignItems: "center", //tiene diferentes propiedades. El predeterminado es "center" (izquiera a derecha)
-    justifyContent: "space-around", //tiene diferentes propiedades. El predeterminado es "center" (arriba a abajo)
+    alignItems: "center",
+    justifyContent: "center",
   },
-
-  text: {
-    color: "yellow",
-    fontSize: 25,
-    //height: 150,
-    // width: 150,
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    marginTop: 20,
   },
-
-  red: { backgroundColor: "red" /*flex: 1*/ },
-  green: { backgroundColor: "green" /*flex: 2*/ },
-  blue: { backgroundColor: "blue" /*flex: 3*/ },
+  countContainer: {
+    alignItems: "center",
+    padding: 10,
+  },
+  countText: {
+    color: "#FF00FF",
+    fontSize: 18,
+  },
 });
