928e04969953b99f7276621f838dd5edf59a358b
ISM42 <133287937+ISM42@users.noreply.github.com>
2024-06-26 17:55:41 -0600

    Práctica 11: finalizada

--- formulario/App.js
+++ formulario/App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 
 export default function App() {
+  const [user, setUser] = useState([]); //definir y asignar usuarios
+  const [loading, setLoading] = useState(true); //definir los estados de carga de la información del usuario (simuulación de la carga)
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={user}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "strech",
+    justifyContent: "center",
+    paddingTop: 40,
+  },
+  item: {
+    padding: 10,
+    fontSize: 24,
+    height: 50,
+    borderColor: "blue",
+    borderBottomWidth: 1,
+  },
+  sectionHeader: {
+    paddingTop: 8,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 8,
+    fontSize: 18,
+    fontWeight: "bold",
+    backgroundColor: "#f2f2f2",
+  },
+  center: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
   },
 });
--- holaMundo/App.js
+++ holaMundo/App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
 import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
